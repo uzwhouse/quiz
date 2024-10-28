2a94369aeb296d8ce5ed98ce9d0fb6d4101be025
uzwhouse <94shakha94@gmail.com>
2024-10-28 13:40:22 +0500

    ovveride readAll

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -2,7 +2,12 @@ package services;
 
 import models.User;
 
+import java.util.LinkedHashSet;
+
 public interface UserService extends CRUDService, Quiz {
+    @Override
+    LinkedHashSet<User> readAll();
+
     User login(String username, String password);
 
     User register(String username, String password);
