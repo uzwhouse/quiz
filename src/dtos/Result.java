b3245804d3fe2072415ed690bc65807fc682aae4
uzwhouse <94shakha94@gmail.com>
2024-10-28 13:37:40 +0500

    override toString

--- src/dtos/Result.java
+++ src/dtos/Result.java
@@ -1,5 +1,8 @@
 package dtos;
 
 public record Result(String question, Answer answer) {
-
+    @Override
+    public String toString() {
+        return "'%s' '%s'%n".formatted(question, answer.isCorrect());
+    }
 }
